e5c05a15da8be022aed940812da83a99b5900ddb
SanjayKumar3110 <sanjaykumar785231@gmail.com>
2025-12-17 19:27:06 +0530

    API updated

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -4,8 +4,8 @@ const instance = axios.create({
   // URL is not specified then it will run on local host
   // If error occurs remove process.env.REACT_APP_API_URL 
   // and keep only "http://127.0.0.1:8000"
-  // baseURL: process.env.REACT_APP_API_URL || "http://127.0.0.1:8000",
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: process.env.REACT_APP_API_URL || "http://127.0.0.1:8000",
+  // baseURL: "http://127.0.0.1:8000",
 });
 
 export default instance;
