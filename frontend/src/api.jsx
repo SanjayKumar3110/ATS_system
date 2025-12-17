9767f757956648f9ea94597d733960788cb2c185
SanjayKumar3110 <sanjaykumar785231@gmail.com>
2025-12-17 11:47:59 +0530

    API updated

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://ats-system-uylp.onrender.com",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default instance;
